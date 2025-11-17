b7a14f2e20d123ea9e6ab7846eecea8af2d5b037
estudosRyan <lucca.ryan.gomes@gmail.com>
2025-11-17 06:47:01 -0300

    Subindo sequelizeErrors com possíveis erros de conexão

---
+++ src/constants/sequelizeErrors.js
@@ -0,0 +1,13 @@
+/**
+* Nomes dos erros de conexão que podem ocorrer no Sequelize 
+*/
+const sequelizeConnectionErrors = [
+    "SequelizeConnectionError",
+    "SequelizeConnectionRefusedError",
+    "SequelizeHostNotFoundError",
+    "SequelizeHostNotReachableError",
+    "SequelizeInvalidConnectionError",
+    "SequelizeConnectionTimedOutError",
+];
+
+module.exports = sequelizeConnectionErrors;
